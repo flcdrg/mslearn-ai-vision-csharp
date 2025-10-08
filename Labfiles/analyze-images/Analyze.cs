497bfdc5edfedf02adb090b1376b907bbe7b5380
David Gardiner <david@gardiner.net.au>
2025-10-08 16:44:22 +1030

    Update console text and format confidence percentages

--- Labfiles/analyze-images/Analyze.cs
+++ Labfiles/analyze-images/Analyze.cs
@@ -1,5 +1,6 @@
 ﻿#:package Azure.AI.Vision.ImageAnalysis@1.0.0
 
+// Import namespaces
 using Azure;
 using Azure.AI.Vision.ImageAnalysis;
 
@@ -9,33 +10,31 @@ var ai_key = Environment.GetEnvironmentVariable("AI_SERVICE_KEY") ?? throw new I
 // Authenticate Azure AI Vision client
 var client = new ImageAnalysisClient(new Uri(ai_endpoint), new AzureKeyCredential(ai_key));
 
-
 // # Analyze image
-
-var result = client.Analyze(new Uri("https://raw.githubusercontent.com/MicrosoftLearning/mslearn-ai-vision/06cf25b6c104c7a45c6b0250702f28ddefc37538/Labfiles/analyze-images/python/image-analysis/images/street.jpg"),
+var result = (await client.AnalyzeAsync(new Uri("https://raw.githubusercontent.com/MicrosoftLearning/mslearn-ai-vision/06cf25b6c104c7a45c6b0250702f28ddefc37538/Labfiles/analyze-images/python/image-analysis/images/street.jpg"),
 VisualFeatures.Caption | VisualFeatures.DenseCaptions | VisualFeatures.Tags | VisualFeatures.Objects | VisualFeatures.People
-).Value;
+)).Value;
 
 // # Get image captions
 if (result.Caption is not null) {
-    Console.WriteLine($"Caption: {result.Caption.Text}, Confidence: {result.Caption.Confidence}");
+    Console.WriteLine("Caption:");
+    Console.WriteLine($" Caption: {result.Caption.Text}, Confidence: {result.Caption.Confidence * 100:F2}%");
 }
 
 if (result.DenseCaptions is not null) {
-    Console.WriteLine("Dense captions in the image:");
+    Console.WriteLine("Dense Captions:");
     foreach (var caption in result.DenseCaptions.Values)
     {
-        Console.WriteLine($"Dense caption: {caption.Text}, Confidence: {caption.Confidence}");
+        Console.WriteLine($" Caption: {caption.Text}, Confidence: {caption.Confidence * 100:F2}%");
     }
 }
 
-
 // # Get image tags
 if (result.Tags is not null) {
-    Console.WriteLine("Tags in the image:");
+    Console.WriteLine("Tags:");
     foreach (var tag in result.Tags.Values)
     {
-        Console.WriteLine($"Tag: {tag.Name}, Confidence: {tag.Confidence}");
+        Console.WriteLine($" Tag: {tag.Name}, Confidence: {tag.Confidence * 100:F2}%");
     }
 }
 
@@ -44,18 +43,19 @@ if (result.Objects is not null) {
     Console.WriteLine("Objects in the image:");
     foreach (var obj in result.Objects.Values)
     {
-        Console.WriteLine($"Object: {obj.Tags[0].Name}, Confidence: {obj.Tags[0].Confidence}");
+        Console.WriteLine($" Object: {obj.Tags[0].Name}, Confidence: {obj.Tags[0].Confidence * 100:F2}%");
     }
 }
 
 // # Get people in the image
-if (result.People is not null) {
+if (result.People is not null)
+{
     Console.WriteLine("People in the image:");
     foreach (var person in result.People.Values)
     {
         if (person.Confidence > 0.2)
         {
-            Console.WriteLine($"{person.BoundingBox} Confidence: {person.Confidence}");
+            Console.WriteLine($" {person.BoundingBox} Confidence: {person.Confidence * 100:F2}%");
         }
     }
 }
