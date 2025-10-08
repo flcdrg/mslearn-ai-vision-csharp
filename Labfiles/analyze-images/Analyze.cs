b8a04ff2f373c5ec86ba506bc69644943c897be4
David Gardiner <david@gardiner.net.au>
2025-10-08 16:26:24 +1030

    Add .NET file and run it from workflow

---
+++ Labfiles/analyze-images/Analyze.cs
@@ -0,0 +1,61 @@
+﻿#:package Azure.AI.Vision.ImageAnalysis@1.0.0
+
+using Azure;
+using Azure.AI.Vision.ImageAnalysis;
+
+var ai_endpoint = Environment.GetEnvironmentVariable("AI_SERVICE_ENDPOINT") ?? throw new InvalidOperationException("Please set the AI_SERVICE_ENDPOINT environment variable.");
+var ai_key = Environment.GetEnvironmentVariable("AI_SERVICE_KEY") ?? throw new InvalidOperationException("Please set the AI_SERVICE_KEY environment variable.");
+
+// Authenticate Azure AI Vision client
+var client = new ImageAnalysisClient(new Uri(ai_endpoint), new AzureKeyCredential(ai_key));
+
+
+// # Analyze image
+
+var result = client.Analyze(new Uri("https://raw.githubusercontent.com/MicrosoftLearning/mslearn-ai-vision/06cf25b6c104c7a45c6b0250702f28ddefc37538/Labfiles/analyze-images/python/image-analysis/images/street.jpg"),
+VisualFeatures.Caption | VisualFeatures.DenseCaptions | VisualFeatures.Tags | VisualFeatures.Objects | VisualFeatures.People
+).Value;
+
+// # Get image captions
+if (result.Caption is not null) {
+    Console.WriteLine($"Caption: {result.Caption.Text}, Confidence: {result.Caption.Confidence}");
+}
+
+if (result.DenseCaptions is not null) {
+    Console.WriteLine("Dense captions in the image:");
+    foreach (var caption in result.DenseCaptions.Values)
+    {
+        Console.WriteLine($"Dense caption: {caption.Text}, Confidence: {caption.Confidence}");
+    }
+}
+
+
+// # Get image tags
+if (result.Tags is not null) {
+    Console.WriteLine("Tags in the image:");
+    foreach (var tag in result.Tags.Values)
+    {
+        Console.WriteLine($"Tag: {tag.Name}, Confidence: {tag.Confidence}");
+    }
+}
+
+// # Get objects in the image
+if (result.Objects is not null) {
+    Console.WriteLine("Objects in the image:");
+    foreach (var obj in result.Objects.Values)
+    {
+        Console.WriteLine($"Object: {obj.Tags[0].Name}, Confidence: {obj.Tags[0].Confidence}");
+    }
+}
+
+// # Get people in the image
+if (result.People is not null) {
+    Console.WriteLine("People in the image:");
+    foreach (var person in result.People.Values)
+    {
+        if (person.Confidence > 0.2)
+        {
+            Console.WriteLine($"{person.BoundingBox} Confidence: {person.Confidence}");
+        }
+    }
+}
--- bicep/main.bicep
+++ bicep/main.bicep
@@ -1,6 +1,7 @@
 param computer_vision_name string = 'cv-computer-vision-eastus-001'
 param location string = resourceGroup().location
 
+// https://learn.microsoft.com/azure/templates/microsoft.cognitiveservices/2025-06-01/accounts?pivots=deployment-language-bicep&WT.mc_id=DOP-MVP-5001655
 resource accounts_cv_computer_vision 'Microsoft.CognitiveServices/accounts@2025-06-01' = {
   name: computer_vision_name
   location: location
